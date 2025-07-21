16aeb6874c8f9d7310306fc6a19b1588a4a4a939
Dennis van Leeuwen <bendora@gmail.com>
2025-07-21 17:27:29 +0200

    Learning Node.js

---
+++ 00_node_basics_ts/src/index.ts
@@ -0,0 +1,5 @@
+import os from "os";
+
+console.log("Hello from TypeScript + Node.js");
+console.log("Your system username:", os.userInfo().username);
+console.log("System uptime (in seconds):", os.uptime());
