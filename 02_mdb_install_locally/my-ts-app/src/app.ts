203c6069ec39deb28fdd1e3dff931f758bd2e3ff
Dennis van Leeuwen <bendora@gmail.com>
2025-07-25 10:45:15 +0200

    Updated m3_Project01

--- 02_mdb_install_locally/my-ts-app/src/app.ts
+++ 02_mdb_install_locally/my-ts-app/src/app.ts
@@ -1,2 +1,13 @@
-const message = "Hello from TypeScript!";
+import { pickRandom } from "#utils";
+
+/* const message = "Hello from TypeScript!";
 console.log(message);
+ */
+
+console.log(pickRandom([1, 2, 3, 4, 5]));
+console.log(pickRandom(["apple", "orange", "banana"]));
+console.log(pickRandom([]));
+console.log(pickRandom([true, false, true]));
+console.log(pickRandom(["red", "green", "blue", "yellow"]));
+console.log(pickRandom([null, undefined, 42]));
+console.log(pickRandom(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]));
